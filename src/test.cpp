5e03e7a64a1813a39936dea9393ba76a0abba002
krishukr-pi <2655290588@qq.com>
2021-05-28 18:04:54 +0800

    test

--- src/test.cpp
+++ src/test.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#define endl "\n"
 
 int main() {
     std::ios::sync_with_stdio(false);
 
     int a, b;
     std::cin >> a >> b;
-    std::cout << a + b << "\n";
+    std::cout << a + b << endl;
 
     return 0;
 }
