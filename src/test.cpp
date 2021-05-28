0eeb83eb73079e289cbcac7bf49df0c566be15fc
krishukr-pi <2655290588@qq.com>
2021-05-28 18:01:36 +0800

    test

--- src/test.cpp
+++ src/test.cpp
@@ -3,7 +3,9 @@
 int main() {
     std::ios::sync_with_stdio(false);
 
-    std::cout << "Hello World!\n";
+    int a, b;
+    std::cin >> a >> b;
+    std::cout << a + b << "\n";
 
     return 0;
 }
