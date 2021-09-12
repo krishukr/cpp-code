ba5a4c87705a05697b9e23c248c8a242402a156f
krishukr <2655290588@qq.com>
2021-09-12 08:36:04 +0800

    0911 com

--- src/LG/lg-P2519.cpp
+++ src/LG/lg-P2519.cpp
@@ -11,9 +11,10 @@ struct Node {
     bool operator<(const Node &n) const {
         return (r == n.r ? l < n.l : r < n.r);
     }
-} q[MAX_N];
+} q[MAX_N], ans[MAX_N];
 
-int cnt, num;
+int w[MAX_N], dp[MAX_N];
+int cnt, num, j = 1;
 
 template <typename T>
 T read();
@@ -36,7 +37,23 @@ int main() {
     std::sort(q + 1, q + cnt + 1);
 
     for (int i = 1; i <= cnt; i++) {
+        int l = q[i].l, r = q[i].r;
+        if (l != q[i - 1].l or r != q[i - 1].r) {
+            num++;
+        }
+        w[num] = std::min(w[num] + 1, r - l + 1);
+        ans[num] = q[i];
     }
+
+    for (int i = 1; i <= n; i++) {
+        dp[i] = dp[i - 1];
+        while (j <= num and ans[j].r == i) {
+            dp[i] = std::max(dp[i], dp[ans[j].l - 1] + w[j]);
+            j++;
+        }
+    }
+
+    std::cout << n - dp[n] << '\n';
 
     return 0;
 }
